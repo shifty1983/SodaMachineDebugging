a968d8b6f3431d739f2007ad1c432bdeaa91f306
Tony Greener <tony.greener@me.com>
2022-04-08 12:58:05 -0700

    Debugging Complete

---
+++ wallet.py
@@ -0,0 +1,20 @@
+import coins
+
+class Wallet:
+    def __init__(self):
+        self.money = []
+        self.fill_wallet()
+
+    def fill_wallet(self):
+        """Method will fill wallet's money list with certain amount of each type of coin when called."""
+        self.money.append
+        for index in range(8):
+            self.money.append(coins.Quarter())
+        for index in range(10):
+            self.money.append(coins.Dime())
+        for index in range(10):
+            self.money.append(coins.Nickel())
+        for index in range(10):
+            self.money.append(coins.Penny())
+
+
